b772a9eea020238317fcf36016ef31d18d92f71a
19CU0209-ozakisora <19cu0209@jec.ac.jp>
2021-03-27 11:18:21 +0900

    データ追加1

---
+++ Game/Source/Chase/ChaseGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "ChaseGameMode.h"
+#include "ChaseCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AChaseGameMode::AChaseGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
---
+++ Game/Source/Chase/ChaseGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ChaseGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AChaseGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AChaseGameMode();
+};
+
+
+
